de35eb0db0c48dfa06eebcad21515b28142a3906
slavikyd <slavikyd@gmail.com>
2025-10-06 10:47:51 +0300

    major: authorization added

---
+++ app/routes/auth.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const { config } = require('../../config/db');
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password)
+      return res.status(400).json({ error: 'Name, email and password are required' });
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ error: 'Email already in use' });
+
+    const user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ error: 'Email and password are required' });
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: 'Invalid credentials' });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
+
+    const payload = { id: user._id, email: user.email };
+    const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '1d' });
+
+    res.json({
+      message: 'Login successful',
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+module.exports = router;
+
+
